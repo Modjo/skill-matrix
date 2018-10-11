3259d9607e54593d7c4e6d9fae92430514923d1d
Florian Bougel <florianbougel@gmail.com>
2018-10-11 15:19:43 +0200

    Updates

---
+++ precache-manifest.363fff978c4f5b79316524be6abce67f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5710a8f500ba0495020",
+    "url": "/skill-matrix/static/js/runtime~main.b5710a8f.js"
+  },
+  {
+    "revision": "122ebd3f40f968720811",
+    "url": "/skill-matrix/static/js/main.122ebd3f.chunk.js"
+  },
+  {
+    "revision": "f4d10aee93265461729a",
+    "url": "/skill-matrix/static/js/1.f4d10aee.chunk.js"
+  },
+  {
+    "revision": "122ebd3f40f968720811",
+    "url": "/skill-matrix/static/css/main.523c0138.chunk.css"
+  },
+  {
+    "revision": "c07f543649b92f08d9f76097ae435d54",
+    "url": "/skill-matrix/index.html"
+  }
+];
