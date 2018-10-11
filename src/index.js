657adf1d96f9b1d1f35fc9e13a96ae2b9e1c346c
Florian Bougel <florianbougel@gmail.com>
2018-10-11 15:16:13 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const SKILLS = [
+  { type: 'JavaScript (ES5/6/7)', level: 95 },
+  { type: 'React/Redux', level: 92 },
+  { type: 'CSS3', level: 90 },
+  { type: 'HTML5', level: 88 },
+  { type: 'Git', level: 80 },
+  { type: 'GraphQL', level: 75 },
+  { type: 'Jest', level: 70 },
+  { type: 'Webpack', level: 65 },
+  { type: 'React Native', level: 60 },
+  { type: 'AWS', level: 50 },
+  { type: 'Node.js', level: 45 },
+  { type: 'Elasticsearch', level: 40 },
+  { type: 'D3', level: 37 },
+  { type: 'Docker', level: 35 }
+];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class SkillBars extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { collapsed: true };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ collapsed: false });
+    }, 100);
+  }
+
+  render() {
+    const { collapsed } = this.state;
+    const { hue, saturation, skills } = this.props;
+
+    return (
+      <div id="app" className={`container ${collapsed ? 'collapsed' : ''}`}>
+        <h1>Skills Matrix (October 2018)</h1>
+        <p>
+          Made with <strong>React</strong> and deployed via{' '}
+          <strong>Github Pages</strong>
+        </p>
+        <hr />
+        <ul className="skills">
+          {skills.map((skill, index) => (
+            <li
+              key={skill.type}
+              style={{
+                width: `${skill.level}%`,
+                backgroundColor: `hsl(${hue}, ${saturation}%, ${100 /
+                  (index + 3.5)}%)`
+              }}
+            >
+              <p>
+                {skill.type}
+                <span>{skill.level}</span>
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <SkillBars hue="194" saturation="100" skills={SKILLS} />,
+  document.getElementById('root')
+);
